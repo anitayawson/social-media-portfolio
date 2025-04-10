dbd2f4887094de1fe8a56f29b7fa18acf890b851
Anita Yawson <anitayawson@gmail.com>
2025-04-10 16:12:25 -0400

    set up file structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import SocialCursor from "../components/SocialCursor";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SocialCursor />
+      <Component {...pageProps} />
+    </>
+  );
 }
