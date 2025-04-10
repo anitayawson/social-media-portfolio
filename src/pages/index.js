52d3db8bc91205572cb1fccf499d4139acc53103
Anita Yawson <anitayawson@gmail.com>
2025-04-10 15:28:23 -0400

    fix tailwind config

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
